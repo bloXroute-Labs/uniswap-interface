cae2e903e1adaaacb8e7bc202791dac3fb5bd63a
Sergey Ilin <ilinserg@gmail.com>
2023-12-01 12:07:20 +0400

    Update origin URL

--- src/graphql/data/apollo.ts
+++ src/graphql/data/apollo.ts
@@ -11,7 +11,7 @@ export const apolloClient = new ApolloClient({
   uri: GRAPHQL_URL,
   headers: {
     'Content-Type': 'application/json',
-    Origin: 'https://app.uniswap.org',
+    Origin: 'https://uni.live',
   },
   cache: new InMemoryCache({
     typePolicies: {
