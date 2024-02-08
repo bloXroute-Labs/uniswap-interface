0fb5396e226685d9a45b54be5c91c23a67b14465
Alexei-Shulga <aleksey-peeranha@bloxroute.com>
2024-02-08 16:01:09 +0300

    new rpc

--- src/hooks/useSwitchRPC.ts
+++ src/hooks/useSwitchRPC.ts
@@ -7,7 +7,7 @@ const chainRPCInfo = {
   1: {
     chainId: `0x${Number(1).toString(16)}`,
     chainName: 'Ethereum bloXroute Protect RPC',
-    rpcUrls: ['https://eth-protect.rpc.blxrbdn.com'],
+    rpcUrls: ['https://swap-live-eth.rpc.blxrbdn.com'],
     nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
     blockExplorerUrls: ['https://etherscan.io/'],
   },
